8ec710373436e651851dce2730e52e32e09e5568
Leo Dion <leogdion@brightdigit.com>
2019-08-27 17:35:06 -0600

    working on ui for connecting

--- heartwitch-ios/heartwitch-ios-app/ContentView.swift
+++ heartwitch-ios/heartwitch-ios-app/ContentView.swift
@@ -7,45 +7,112 @@
 
 import SwiftUI
 import Network
+import WatchConnectivity
 
 var globalConnection : NWConnection?
 var sourceTimer : DispatchSourceTimer?
 
 let jsonEncoder = JSONEncoder()
+
+class SessionManager : NSObject, ObservableObject, WCSessionDelegate {
+  @Published var session : WCSession? {
+    didSet {
+      self.objectWillChange.send()
+    }
+  }
+  
+  @Published var identifer : UUID? {
+    didSet {
+      self.objectWillChange.send()
+    }
+  }
+  
+  var connectable : Bool {
+    return self.session != nil && self.identifer == nil
+  }
+  func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
+    switch (activationState) {
+    case .activated:
+      self.session = session
+    case .notActivated:
+      self.session = nil
+    case .inactive:
+      self.session = nil
+    @unknown default:
+      self.session = nil
+    }
+    
+  }
+  
+  
+  
+  func sessionDidBecomeInactive(_ session: WCSession) {
+    self.session = nil
+  }
+  
+  func sessionDidDeactivate(_ session: WCSession) {
+    self.session = nil
+  }
+  
+  public func postIdentifier(_ identifier: UUID) {
+    let data = withUnsafePointer(to: identifier) {
+        Data(bytes: $0, count: MemoryLayout.size(ofValue:  identifier))
+    }
+    
+    self.session!.sendMessageData(data, replyHandler: { (_) in
+      self.identifer = identifier
+    }) { (error) in
+      print(error)
+    }
+  }
+  
+  override init () {
+    super.init()
+    if WCSession.isSupported() {
+      WCSession.default.delegate = self
+      WCSession.default.activate()
+    }
+  }
+}
 struct ContentView : View {
   @State var id: String = ""
+  @ObservedObject var sessionManager = SessionManager()
   
   func beginConnect (){
-    
-    guard let uuid = UUID(uuidString: self.id) else {
-      return
+    if let identifier = UUID(uuidString: id) {
+      self.sessionManager.postIdentifier(identifier)
     }
-    guard let url = URL(string: "ws://localhost:8080/workouts/\(uuid)/run") else {
-      return
-    }
-    let task = URLSession.shared.webSocketTask(with: url)
-    
-    task.resume()
-    let source = DispatchSource.makeTimerSource()
-    
-    source.setEventHandler {
-      let wkData = WorkoutData(heartRate: Double.random(in: 70...160))
-      if let data = try? jsonEncoder.encode(wkData) {
-        
-        task.send(.data(data)) { (error) in
-          print(error)
-        }
-      }
-    }
-    source.schedule(deadline: .now(), repeating: 5)
-    source.activate()
-    sourceTimer = source
+//    WCSession.default.sendMessage(["identifer": self.id], replyHandler: { (response) in
+//
+//    }) { (error) in
+//
+//    }
+//    guard let uuid = UUID(uuidString: self.id) else {
+//      return
+//    }
+//    guard let url = URL(string: "ws://localhost:8080/workouts/\(uuid)/run") else {
+//      return
+//    }
+//    let task = URLSession.shared.webSocketTask(with: url)
+//
+//    task.resume()
+//    let source = DispatchSource.makeTimerSource()
+//
+//    source.setEventHandler {
+//      let wkData = WorkoutData(heartRate: Double.random(in: 70...160))
+//      if let data = try? jsonEncoder.encode(wkData) {
+//
+//        task.send(.data(data)) { (error) in
+//          print(error)
+//        }
+//      }
+//    }
+//    source.schedule(deadline: .now(), repeating: 5)
+//    source.activate()
+//    sourceTimer = source
   }
   
   
-  func connectionReady(_ connection: NWConnection) {
-    
-  }
   
   var body: some View {
     VStack{
@@ -53,6 +120,12 @@ struct ContentView : View {
       Button(action: self.beginConnect) {
         Text("Connect")
       }
+    }
+  }
+  
+  var connectedView : some View {
+    sessionManager.identifer.map{_ in
+      Text("Connected")
     }
   }
 }
--- heartwitch-ios/heartwitch-watchOS-app Extension/ContentView.swift
+++ heartwitch-ios/heartwitch-watchOS-app Extension/ContentView.swift
@@ -6,15 +6,76 @@
 //
 
 import SwiftUI
+import WatchConnectivity
+
+class SessionManager : NSObject, ObservableObject, WCSessionDelegate {
+  @Published var session : WCSession? {
+    didSet {
+      self.objectWillChange.send()
+    }
+  }
+  @Published var identifier : UUID? {
+    didSet {
+      print("indentified")
+      self.objectWillChange.send()
+    }
+  }
+  func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
+    switch (activationState) {
+    case .activated:
+      self.session = session
+      print("activated")
+    case .notActivated:
+      self.session = nil
+    case .inactive:
+      self.session = nil
+    @unknown default:
+      self.session = nil
+    }
+    
+  }
+  
+  
+  override init () {
+    super.init()
+    if WCSession.isSupported() {
+      print("activating")
+      WCSession.default.delegate = self
+      WCSession.default.activate()
+      
+    }
+  }
+  
+  func session(_ session: WCSession, didReceiveMessageData messageData: Data, replyHandler: @escaping (Data) -> Void) {
+    
+    
+    var bytes = [UInt8].init(repeating: 0, count: messageData.count)
+    messageData.copyBytes(to: &bytes, count: messageData.count)
+    let uuid = NSUUID(uuidBytes: bytes) as UUID
+    self.identifier = uuid
+    replyHandler(Data())
+  }
+  
+  
+}
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello World")
+  @ObservedObject var sessionManager = SessionManager()
+  
+  var body: some View {
+    ZStack{
+      sessionManager.identifier.map{_ in
+        Text("Connected")
+      }
+      Text("Connecting...")
     }
+    
+  }
+  
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
