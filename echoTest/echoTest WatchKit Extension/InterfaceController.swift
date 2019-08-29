237a901332177e05ba2f070c0e7f6d9d71cc32a2
Leo Dion <leogdion@brightdigit.com>
2019-08-28 19:34:03 -0600

    working http post

--- echoTest/echoTest WatchKit Extension/InterfaceController.swift
+++ echoTest/echoTest WatchKit Extension/InterfaceController.swift
@@ -9,15 +9,41 @@
 import WatchKit
 import Foundation
 
+struct Post : Codable {
+  let id : Int?
+  let title: String
+  let body : String
+  let userId : Int
+}
 
 class InterfaceController: WKInterfaceController {
+  let jsonEncoder = JSONEncoder()
+  let jsonDecoder = JSONDecoder()
   
   override func awake(withContext context: Any?) {
     super.awake(withContext: context)
     
     // Configure interface objects here.
-    let webSocketTask = URLSession.shared.webSocketTask(with: URL(string: "wss://echo.websocket.org")!)
-    webSocketTask.resume()
+    let timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { (timer) in
+      var urlRequest = URLRequest(url: URL(string: "https://jsonplaceholder.typicode.com/posts")!)
+      urlRequest.httpBody = try! self.jsonEncoder.encode(Post(id: nil, title: "test", body: "test", userId: Int.random(in: 1...300)))
+      urlRequest.httpMethod = "POST"
+      urlRequest.addValue("application/json; charset=UTF-8", forHTTPHeaderField: "Content-type")
+      let task = URLSession.shared.dataTask(with: urlRequest, completionHandler: self.onResponse)
+      task.resume()
+    }
+  }
+  
+  func onResponse(data: Data?, response: URLResponse?, error: Error?) {
+    if let error = error {
+      print(error)
+      return
+    }
+    guard let data = data else {
+      return
+    }
+    let post = try! jsonDecoder.decode(Post.self, from: data)
+    print(post.id)
   }
   
   override func willActivate() {
